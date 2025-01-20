7a234522b0396da45d2ed3ebb653a6e2d8f9ce89
George <georgesamirayead@gmail.com>
2025-01-20 20:05:08 +0200

    +Add Splash Screen +Developed Home Screen +add 4 fragments

---
+++ app/src/main/java/com/example/islamk/fragments/TasbeehFragment.kt
@@ -0,0 +1,24 @@
+package com.example.islamk.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.islamk.databinding.FregmentTasbeehBinding
+
+class TasbeehFragment : Fragment() {
+    lateinit var binding: FregmentTasbeehBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FregmentTasbeehBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/islamk/home/HomeActivity.kt
+++ app/src/main/java/com/example/islamk/home/HomeActivity.kt
@@ -1,20 +1,27 @@
-package com.example.islamk
+package com.example.islamk.home
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.islamk.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        window.insetsController?.hide(WindowInsets.Type.statusBars())
+
     }
 }
